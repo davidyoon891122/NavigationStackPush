51380f763e8c51d2c8b29ac478194b688a70dba9
Davidyoon <davidyoon@koreainvestment.com>
2023-10-17 12:15:20 +0900

    Initial Commit

---
+++ NavigationPath/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NavigationPath
+//
+//  Created by 112751 on 2023/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationPath/NavigationPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPathApp.swift
+//  NavigationPath
+//
+//  Created by 112751 on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
